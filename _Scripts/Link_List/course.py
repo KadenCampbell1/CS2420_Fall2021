a4501282b4c60f21a48a5c35b34b8d7dc45885d6
Kaden(LenLap) <10836246@my.uvu.edu>
2021-10-05 15:56:25 -0600

    worked on course I think it is done; added test file to repo

--- _Scripts/Link_List/course.py
+++ _Scripts/Link_List/course.py
@@ -1,9 +1,24 @@
 class Course:
-    def __init__(self, course_number=0, course_name="", credit_hrs=0, grades=0):
-        self.course_number = course_number
-        self.course_name = course_name
-        self.credit_hrs = credit_hrs
-        self.grades = grades
+    def __init__(self, course_number: int = 0, course_name: str = "",
+                 credit_hrs: float = 0.0, grades: float = 0.0, next=None):
+        if type(course_number) == int and course_number >= 0:
+            self.course_number = course_number
+        else:
+            raise ValueError
+        if type(course_name) == str:
+            self.course_name = course_name
+        else:
+            raise ValueError
+        if type(credit_hrs) == float and credit_hrs >= 0:
+            self.credit_hrs = credit_hrs
+        else:
+            raise ValueError
+        if type(grades) == float and grades >= 0:
+            self.grades = grades
+        else:
+            raise ValueError
+
+        self.next = next
 
     def number(self):
         return int(self.course_number)
@@ -17,8 +32,9 @@ class Course:
     def grade(self):
         return float(self.grades)
 
+    def set_next(self, next):
+        self.next = next
+
     def __str__(self):
         return f"cs{self.course_number} {self.course_name} Grade: {self.grades:0.1f} " \
                f"Credit Hours: {self.credit_hrs:0.1f}"
-
-    # needs next variable?
---
+++ _Scripts/test.py
@@ -0,0 +1,13 @@
+def main():
+    value = 10
+    if type(value) == int:
+        print("is int")
+
+    if type(value) != int:
+        print("is not int")
+
+    print(type(value))
+
+
+if __name__ == "__main__":
+    main()
