8aac1b35027d4a1d9142bbf2f569369dd17608fa
Kaden(LenLap) <10836246@my.uvu.edu>
2021-11-15 15:56:56 -0700

    Worked on dictionary project
    changed pyramid.py to use hashmap and began completing functions in hashmap.py

--- _Scripts/Dictionaries/hashmap.py
+++ _Scripts/Dictionaries/hashmap.py
@@ -4,6 +4,7 @@ __init__ arguments:
 None
 
 Functions:
+calculation(key) -- returns the index for the values of key
 get(key) -- returns the value for key if key is in the hashmap.
 set(key, value) -- adds the (key, value) pair to the hashmap.
 remove(key) -- removes the key and its associated value from the map.
@@ -18,6 +19,16 @@ __str__() -- returns a list of key-value pairs.
 class HashMap:
     def __init__(self):
         self.capacity = 7
+        self.key_lyst = [None] * self.capacity
+        self.value_lyst = [None] * self.capacity
+
+    def calculation(self, key):
+        """returns the index for the values of key in the hashmap
+
+        arguments:
+        key -- type Any
+        """
+        pass
 
     def get(self, key):
         """returns the value for key if key is in the hashmap
@@ -27,7 +38,10 @@ class HashMap:
         arguments:
         key -- type Any
         """
-        pass
+        if key in self.key_lyst:
+            # calculate index of value_lyst and return value at index
+            pass
+        raise KeyError("Key is not in HashMap")
 
     def set(self, key, value):
         """adds the (key, value) pair to the hashmap
@@ -39,7 +53,10 @@ class HashMap:
         key -- type Any
         value -- type Any
         """
+
         # rehash when load-factor is >= 80%. new load-factor should be k=2k-1
+        # If calculation give a collision value rewrite calculation in here using probing
+        # save what values have new locations
         pass
 
     def remove(self, key):
@@ -51,24 +68,36 @@ class HashMap:
         arguments:
         key -- type Any
         """
-        pass
+
+        self.value_lyst.pop(self.get(key))
+        self.key_lyst.pop(key)
 
     def clear(self):
         """empties the hashmap of all key-values"""
-        pass
+
+        self.capacity = 7
+        self.key_lyst = []
+        self.value_lyst = [None] * self.capacity
 
     def capacity(self):
         """returns the current capacity--number of buckets--in the map"""
+
         return self.capacity
 
     def size(self):
         """returns the number of key-value pairs in the map"""
-        pass
+
+        return len(self.key_lyst)
 
     def keys(self):
         """returns a list of keys"""
-        pass
+
+        return self.key_lyst
 
     def __str__(self):
-        """returns a list of key-value pairs"""
-        return "return list of key-value pairs"
+        """returns a string list of key-value pairs"""
+
+        temp = []
+        for i in self.key_lyst:
+            temp.append((i, self.get(i)))
+        return str(temp)
--- _Scripts/Dictionaries/pyramid.py
+++ _Scripts/Dictionaries/pyramid.py
@@ -7,8 +7,10 @@ Date: 11/11/2021
 
 import sys
 from time import perf_counter
+from hashmap import HashMap
 
-cache = {(0, 0): 0}
+cache = HashMap()
+cache.set((0, 0), 0)
 cache_hit = 0
 function_calls = 0
 
@@ -20,6 +22,78 @@ def weight_on(r, c):
     r -- type int
     c -- type int
     """
+
+    value = None
+    global function_calls
+    function_calls += 1
+    if cache.get((r, c)):
+        global cache_hit
+        cache_hit += 1
+        return cache.get((r, c))
+    else:
+        if r == 0 and c == 0:
+            cache.set((r, c), 0)
+            return 0
+        if c == 0:
+            value = float((weight_on(r - 1, c) + 200) / 2)
+            cache.set((r, c), value)
+        if c == r:
+            value = float((weight_on(r - 1, c - 1) + 200) / 2)
+            cache.set((r, c), value)
+        if c != 0 and c != r:
+            value = float(((weight_on(r - 1, c) + 200) / 2) + ((weight_on(r - 1, c - 1) + 200) / 2))
+            cache.set((r, c), value)
+        cache.set((r, c), value)
+    return value
+
+
+def main():
+    cache.clear()
+    value = 7  # sys.argv[1]  # pyramid.py <number> "this is how to use sys.argv[1] in cmd"
+    # with open("part3.txt", "w") as OUT_FILE:
+    #     start = perf_counter()
+    #     for i in range(int(value)):
+    #         for x in range(0, i + 1):
+    #             OUT_FILE.write(f"{weight_on(i, x):0.2f} ")
+    #         OUT_FILE.write("\n")
+    #
+    #     end = perf_counter()
+    #     time = end - start
+    #     OUT_FILE.write(f"\nElapsed time: {time} seconds\n"
+    #                    f"Number of function calls: {function_calls}\n"
+    #                    f"Number of cache calls: {cache_hit}")
+
+    cache_temp = HashMap()
+    print(cache_temp)
+
+
+if __name__ == "__main__":
+    main()
+
+# previous setup
+"""
+Project 6: Hashing and Caching
+Author: Kaden Campbell
+Course: CS2420
+Date: 11/11/2021
+
+
+import sys
+from time import perf_counter
+from hashmap import HashMap
+
+cache = {(0, 0): 0}
+cache_hit = 0
+function_calls = 0
+
+
+def weight_on(r, c):
+    returns the weight on the item in row r and column c
+
+    arguments:
+    r -- type int
+    c -- type int
+    
 
     value = None
     global function_calls
@@ -48,19 +122,23 @@ def weight_on(r, c):
 def main():
     cache.clear()
     value = 7  # sys.argv[1]  # pyramid.py <number> "this is how to use sys.argv[1] in cmd"
-    with open("part3.txt", "w") as OUT_FILE:
-        start = perf_counter()
-        for i in range(int(value)):
-            for x in range(0, i + 1):
-                OUT_FILE.write(f"{weight_on(i, x):0.2f} ")
-            OUT_FILE.write("\n")
+    # with open("part3.txt", "w") as OUT_FILE:
+    #     start = perf_counter()
+    #     for i in range(int(value)):
+    #         for x in range(0, i + 1):
+    #             OUT_FILE.write(f"{weight_on(i, x):0.2f} ")
+    #         OUT_FILE.write("\n")
+    #
+    #     end = perf_counter()
+    #     time = end - start
+    #     OUT_FILE.write(f"\nElapsed time: {time} seconds\n"
+    #                    f"Number of function calls: {function_calls}\n"
+    #                    f"Number of cache calls: {cache_hit}")
 
-        end = perf_counter()
-        time = end - start
-        OUT_FILE.write(f"\nElapsed time: {time} seconds\n"
-                       f"Number of function calls: {function_calls}\n"
-                       f"Number of cache calls: {cache_hit}")
+    cache_temp = HashMap()
+    print(cache_temp)
 
 
 if __name__ == "__main__":
     main()
+"""
